868cbbcbceb1b661b809cebb6bbb4b110421848e
Allen Chrisso Dhas <allencdvja@gmail.com>
2024-10-30 00:51:18 +0530

    integrated fiberv2

--- main.go
+++ main.go
@@ -1,9 +1,91 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// Item represents the object we'll store in MongoDB
+type Item struct {
+	ID          string    `json:"id,omitempty" bson:"_id,omitempty"`
+	Name        string    `json:"name" bson:"name"`
+	Description string    `json:"description" bson:"description"`
+	CreatedAt   time.Time `json:"created_at" bson:"created_at"`
+}
+
+var client *mongo.Client
+var collection *mongo.Collection
+
 func main() {
-	fmt.Println("Rest API Boilerplate")
+	// Set up MongoDB connection
+	ctx := context.Background()
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	var err error
+	client, err = mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	// Get collection reference
+	collection = client.Database("testdb").Collection("items")
+
+	// Create Fiber app
+	app := fiber.New()
+
+	// Routes
+	app.Get("/items", getItems)
+	app.Post("/items", createItem)
+
+	// Start server
+	log.Fatal(app.Listen(":8080"))
+}
+
+func getItems(c *fiber.Ctx) error {
+	ctx := context.Background()
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	defer cursor.Close(ctx)
+
+	var items []Item
+	if err = cursor.All(ctx, &items); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.JSON(items)
+}
+
+func createItem(c *fiber.Ctx) error {
+	var item Item
+	if err := c.BodyParser(&item); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	item.CreatedAt = time.Now()
+
+	ctx := context.Background()
+	result, err := collection.InsertOne(ctx, item)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"id": result.InsertedID,
+	})
 }
